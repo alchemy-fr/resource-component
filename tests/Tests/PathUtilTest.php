2d02b0f4a787fbae578bb48948757792383f5dff
Thibaud Fabre <thibaud@aztech.io>
2016-03-21 22:41:51 +0100

    Add path utilities tests

---
+++ tests/Tests/PathUtilTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Alchemy\Tests\Resource;
+
+use Alchemy\Resource\PathUtil;
+
+class PathUtilTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function getPathsAndMatchingBaseNames()
+    {
+        return [
+            [ '/absolute/path/to/file', 'file' ],
+            [ '/absolute/path/to/file/with/extension.ext', 'extension.ext' ],
+            [ 'relative/path/to/file', 'file' ],
+            [ 'relative/path/to/file/with/extension.ext', 'extension.ext' ],
+            [ 'pathless-file', 'pathless-file' ],
+            [ 'pathless-file-with-extension.ext', 'pathless-file-with-extension.ext' ]
+        ];
+    }
+
+    /**
+     * @dataProvider getPathsAndMatchingBaseNames
+     * @param string $path
+     * @param string $expectedBaseName
+     */
+    public function testGetBasenameExtractsBasenameFromPath($path, $expectedBaseName)
+    {
+        $this->assertEquals($expectedBaseName, PathUtil::basename($path));
+    }
+
+    public function getPathsAndMatchingExtensions()
+    {
+        return [
+            [ 'file.ext', 'ext' ],
+            [ '/absolute/path/to/file/with/extension.ext', 'ext' ],
+            [ 'relative/path/to/file', '' ],
+            [ 'relative/path/to/file/with/extension.ext', 'ext' ],
+            [ 'pathless-and-extensionless-file', '' ],
+            [ 'pathless-file-with-extension.ext', 'ext' ],
+            [ 'file-with-complex-extension.tar.gz', 'gz']
+        ];
+    }
+
+    /**
+     * @dataProvider getPathsAndMatchingExtensions
+     *
+     * @param string $path
+     * @param string $expectedExtension
+     */
+    public function testGetExtensionExtractsExtension($path, $expectedExtension)
+    {
+        $this->assertEquals($expectedExtension, PathUtil::extractExtension($path));
+    }
+}
